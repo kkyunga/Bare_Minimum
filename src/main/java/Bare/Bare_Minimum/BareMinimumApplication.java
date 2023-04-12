5c47bb64b5a4a6634e9512d82a8ea76aed0499c9
kkyunga <rlaruddk1020@naver.com>
2023-04-12 17:59:18 +0900

    수정

---
+++ src/main/java/Bare/Bare_Minimum/BareMinimumApplication.java
@@ -0,0 +1,13 @@
+package Bare.Bare_Minimum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareMinimumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareMinimumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Bare/Bare_Minimum/BareMinimumApplicationTests.java
@@ -0,0 +1,13 @@
+package Bare.Bare_Minimum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BareMinimumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
